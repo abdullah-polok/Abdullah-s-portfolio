c3fd82c0c7ecd9c46e794e780a4abae6a68884c2
abdullah-polok <abdullahalrahman23@gmail.com>
2025-07-19 13:50:56 +0300

    Fix:text layout in home banner

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -13,11 +13,14 @@ const Home = () => {
         <div className="hero-content text-neutral-content text-center">
           <div className="max-w-full">
             <h1 className="mb-5 text-7xl font-bold">I'm Abdullah Al Rahman</h1>
-            <p className="mb-5 text-xl max-w-4xl text-center">
-              I'm a Computer Science and Cybersecurity student at the University
-              of Kurdistan Hewlêr, specializing in front-end development with
-              React. I build responsive, user-friendly web apps with a focus on
-              performance and security.
+            <p className="mb-5 text-xl max-w-4xl text-center text-gray-400">
+              I'm a Computer Science and Cybersecurity student at the{" "}
+              <span className="text-white font-semibold">
+                University of Kurdistan Hewlêr
+              </span>
+              , specializing in front-end development with{" "}
+              <span className="text-white">React</span>. I build responsive,
+              user-friendly web apps with a focus on performance and security.
             </p>
           </div>
         </div>
